dd495bbd73eefb2710f4eae408c0a2933b564f03
Debasish Rana <debasish3912@gmail.com>
2026-04-18 18:28:33 +0530

    added the menu overlay

--- home.js
+++ home.js
@@ -1,3 +1,21 @@
-    function myFunction(){
-        var input;
-    }
+
+function openMenu(){
+  document.getElementById("menu").classList.add("active");
+}
+
+function closeMenu(){
+  document.getElementById("menu").classList.remove("active");
+}
+
+function toggleDarkmode(){
+  document.body.classList.toggle("dark-mode");
+
+  const isDark = document.body.classList.contains("dark-mode");
+  const logo = document.getElementById("logo");
+
+  if (isDark) {
+    logo.src = "images/logo1.png";
+  } else {
+    logo.src = "images/logo3.png";
+  }
+}
